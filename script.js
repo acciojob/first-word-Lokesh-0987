6253fe672012bf985e9f3843a86cad3676575176
Lokesh Paleti <145462502+Lokesh-0987@users.noreply.github.com>
2026-03-02 23:08:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 function firstWord(s) {
-  // your code here
+  if (!s || s.length === 0) {
+    return "";
+  }
+
+  let i = 0;
+
+  // Skip leading spaces
+  while (i < s.length && s[i] === " ") {
+    i++;
+  }
+
+  // If string had only spaces
+  if (i === s.length) {
+    return "";
+  }
+
+  let start = i;
+
+  // Move until first space or end of string
+  while (i < s.length && s[i] !== " ") {
+    i++;
+  }
+
+  return s.slice(start, i);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
